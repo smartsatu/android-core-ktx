259ebe7f10adff9ee0c52e93f25dbf20e2799bab
Daniil Lukashevich <danaimset@gmail.com>
2019-07-22 23:47:35 +0300

    [context] Broadcast extensions

--- core-ktx/src/main/java/com/smartsatu/android/core/content/Context.kt
+++ core-ktx/src/main/java/com/smartsatu/android/core/content/Context.kt
@@ -2,8 +2,12 @@ package com.smartsatu.android.core.content
 
 import android.accounts.AccountManager
 import android.app.Application
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -19,6 +23,17 @@ fun Context.getApp() = this.applicationContext as Application
 fun Context.inflater() = LayoutInflater.from(this)
 
 fun Context.broadcasts() = LocalBroadcastManager.getInstance(this)
+
+fun Context.broadcastSync(action: String, extras: Bundle? = null) = broadcastSync(Intent(action), extras)
+
+fun Context.broadcastSync(intent: Intent, extras: Bundle? = null) = broadcasts()
+        .sendBroadcastSync(intent.apply { extras?.let { putExtras(extras) } })
+
+fun Context.register(receiver: BroadcastReceiver, vararg actions: String) = LocalBroadcastManager.getInstance(this)
+        .registerReceiver(receiver, IntentFilter().apply { actions.forEach { addAction(it) } })
+
+fun Context.unregister(receiver: BroadcastReceiver) = LocalBroadcastManager.getInstance(this)
+        .unregisterReceiver(receiver)
 
 fun Context.am() = getSystemService(Context.ACCOUNT_SERVICE) as AccountManager
 
